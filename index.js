89928b6ebc22d211556b44a566132260e05e6444
Tanvir Singh <tanveerkhalsa@gmail.com>
2019-08-26 07:06:57 +0530

    Init Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+//Import Routes
+const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
+
+dotenv.config()
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true}, () => {
+    console.log('connected to DB...')
+})
+
+//Middleware
+app.use(express.json())
+
+//Routes Middleware
+app.use('/api/user', authRoute)
+app.use('/api/posts', postRoute)
+
+app.listen(3000, () => {
+    console.log('Server Up and Running...')
+})
